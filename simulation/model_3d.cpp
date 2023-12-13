67173fa12000a3f369a9bc9d896dedbd068dffc5
Igor Gribanov <igor1215@gmail.com>
2023-12-12 21:13:55 -0330

    working

--- simulation/model_3d.cpp
+++ simulation/model_3d.cpp
@@ -68,39 +68,45 @@ void icy::Model3D::Reset()
     compute_time_per_cycle = 0;
     indenter_force_history.clear();
 
-    const real &bx = prms.
-    const real &by = prms.BlockHeight;
+    const real &bx = prms.IceBlockDimX;
+    const real &by = prms.IceBlockDimY;
+    const real &bz = prms.IceBlockDimZ;
+    const real &bvol = bx*by*bz;
     const real &h = prms.cellsize;
+    const real &magic_constant = 0.25;
 
-    const real kRadius = pow() sqrt(block_length*block_height/(prms.PointsWanted*(0.5*SimParams::pi)*100./97.));
-    const std::array<real, 2>kXMin{5.0*h, 2.0*h};
-    const std::array<real, 2>kXMax{5.0*h+block_length, 2.0*h+block_height};
+    const real z_center = prms.GridZ*h/2;
+    const real block_z_min = max(z_center - bz/2, 0);
+    const real bloc_z_max = min(z_center + bz/2, prms.GridZ*h);
+    const real kRadius =cbrt(magic_constant*bvol/prms.PointsWanted);
+    const std::array<real, 3>kXMin{5.0*h, 2.0*h, block_z_min};
+    const std::array<real, 3>kXMax{5.0*h+block_length, 2.0*h+block_height, block_z_max};
+
     spdlog::info("starting thinks::PoissonDiskSampling");
-    std::vector<std::array<real, 2>> prresult = thinks::PoissonDiskSampling(kRadius, kXMin, kXMax);
-    spdlog::info("finished thinks::PoissonDiskSampling; {} ", prms.nPts);
+    std::vector<std::array<real, 3>> prresult = thinks::PoissonDiskSampling(kRadius, kXMin, kXMax);
     prms.nPts = prresult.size();
+    spdlog::info("finished thinks::PoissonDiskSampling; {} ", prms.nPts);
     points.resize(prms.nPts);
 
-    prms.ParticleVolume = block_length*block_height/prms.nPts;
+    prms.ParticleVolume = bvol/prms.nPts;
     prms.ParticleMass = prms.ParticleVolume*prms.Density;
     for(int k = 0; k<prms.nPts; k++)
     {
-        Point &p = points[k];
+        Point3D &p = points[k];
         p.Reset();
-        p.pos[0] = prresult[k][0];
-        p.pos[1] = prresult[k][1];
+        for(int i=0;i<3;i++) p.pos[i] = prresult[k][i];
         p.pos_initial = p.pos;
     }
     prms.indenter_y = block_height + 2*h + prms.IndDiameter/2 - prms.IndDepth;
     prms.indenter_x = prms.indenter_x_initial = 4*h - prms.IndDiameter/2;
 
-    gpu.cuda_allocate_arrays(prms.GridX*prms.GridY, prms.nPts);
+    gpu.cuda_allocate_arrays(prms.GridTotal, prms.nPts);
     gpu.transfer_ponts_to_device(points);
     Prepare();
     spdlog::info("icy::Model::Reset() done");
 }
 
-void icy::Model::ResetToStep0()
+void icy::Model3D::ResetToStep0()
 {
     spdlog::info("ResetToStep0()");
 
@@ -115,13 +121,12 @@ void icy::Model::ResetToStep0()
     prms.indenter_y = prms.BlockHeight + 2*h + prms.IndDiameter/2 - prms.IndDepth;
     prms.indenter_x = prms.indenter_x_initial = 5*h - prms.IndDiameter/2 - h;
     gpu.transfer_ponts_to_device(points);
-    gpu.cuda_update_constants();
     Prepare();
     spdlog::info("ResetToStep0() done");
 }
 
 
-void icy::Model::Prepare()
+void icy::Model3D::Prepare()
 {
     spdlog::info("icy::Model::Prepare()");
     abortRequested = false;
